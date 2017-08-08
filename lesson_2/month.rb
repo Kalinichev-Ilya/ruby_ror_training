fcf539bf544a2e202f1b01ad74777facef42d205
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-08 21:22:32 +0300

    hash of months

---
+++ lesson_2/month.rb
@@ -0,0 +1,20 @@
+# Make a hash that contains months and the number
+# of days in a month. In the cycle, output those
+# months, in which the number of days is exactly 30.
+puts 'Task 1'
+
+months = {
+  January: 31,
+  February: 29,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+}
+months.each { |month, days| puts "#{month}: #{days}" if days == 30 }
