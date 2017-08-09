a3eb29800a05fca6f05e5acbdc623aefbbf7f2a7
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-10 00:11:27 +0300

    route entity implementation

--- lesson_3/route.rb
+++ lesson_3/route.rb
@@ -1,3 +1,22 @@
+# Route entity
 class Route
-  
+  def initialize(departure, arrival, stations = [])
+    @departure = departure
+    @arrival = arrival
+    @stations = stations
+  end
+
+  def add(station)
+    @stations << station
+  end
+
+  def delete(station)
+    @stations.delete station
+  end
+
+  def stations
+    @stations.unshift @departure
+    @stations << @arrival
+    @stations
+  end
 end
