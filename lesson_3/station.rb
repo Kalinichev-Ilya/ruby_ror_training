fc05b28eeef9ffadae8f0e6a66fe2c24ee47af7c
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-10 00:11:47 +0300

    station entity implementation

--- lesson_3/station.rb
+++ lesson_3/station.rb
@@ -1,3 +1,34 @@
+# Station entity
 class Station
+  attr_reader :name
 
+  def initialize(name, trains = [])
+    @name = name
+    @trains = trains
+  end
+
+  # add train
+  def add_train(train)
+    @trains << train
+  end
+
+  # return list of trains
+  def train_list
+    @trains
+  end
+
+  # return list of trains by type
+  def trains_by_type
+    cargo = []
+    passenger = []
+    @trains.each do |train|
+      cargo << train if train.type == 'cargo'
+      passenger << train if train.type == 'passenger'
+    end
+    { cargo: cargo, passenger: passenger }
+  end
+
+  def move(train)
+    @trains.delete train
+  end
 end
