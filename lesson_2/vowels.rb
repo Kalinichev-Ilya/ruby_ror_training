b4af1434b62cbda0f98dd980e46a88ce37ed8ebe
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-09 10:55:03 +0300

    deleted superfluous logic

--- lesson_2/vowels.rb
+++ lesson_2/vowels.rb
@@ -1,22 +1,12 @@
 # Fill the hash with a vowel where value is the
 # alphabetic sequence number of the letter (a - 1).
 
-# gets array of all chars alphabet
 chars = ('a'..'z').to_a
+vowels = %w[a e i o]
 
-# get numbers of chars
-i = 0
 alphabet = {}
-chars.each do |char|
-  alphabet[char] = i
-  i += 1
+chars.each_with_index do |char, i|
+  alphabet[char] = i if vowels.include? char
 end
 
-# delete unnecessary chars
-result = {}
-%w[a e i o].each do |char|
-  alphabet.include? char
-  result[char] = alphabet[char]
-end
-
-result.each { |key, val| puts "#{key}: #{val}" }
+alphabet.each { |key, val| puts "#{key}: #{val}" }
