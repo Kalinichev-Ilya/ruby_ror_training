54ea24dc80b05222ddd2dca1d20971f793ccb6d4
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-09-01 00:34:13 +0300

    code style fixes

--- lesson_8/app/exceptions/validation_error.rb
+++ lesson_8/app/exceptions/validation_error.rb
@@ -1,7 +1,7 @@
 # Custom validation exception
 class ValidationError < StandardError
   attr_reader :obj
-  
+
   def initialize(obj, message)
     @obj = obj
     super(message)
--- lesson_8/program.rb
+++ lesson_8/program.rb
@@ -5,7 +5,7 @@ class Program
     @stations = []
     @trains = []
   end
-  
+
   def run
     loop do
       puts 'Enter q for quit'
@@ -21,35 +21,35 @@ class Program
       puts '9. Show wagon list by train'
       puts '10. Take the place in train wagon'
       input = gets.chomp
-      
+
       case input
-        when '1'
-          create_train_interface
-        when '2'
-          create_route_and_station_management_interface
-        when '3'
-          assign_route_to_train_interface
-        when '4'
-          add_wagon_to_train_interface
-        when '5'
-          remove_wagon_from_train_interface
-        when '6'
-          move_train_on_the_route_interface
-        when '7'
-          show_station_list_interface
-        when '8'
-          show_trains_on_station_by_type_interface
-        when '9'
-          show_wagon_list_by_train
-        when '10'
-          take_the_place_in_train_wagon
+      when '1'
+        create_train_interface
+      when '2'
+        create_route_and_station_management_interface
+      when '3'
+        assign_route_to_train_interface
+      when '4'
+        add_wagon_to_train_interface
+      when '5'
+        remove_wagon_from_train_interface
+      when '6'
+        move_train_on_the_route_interface
+      when '7'
+        show_station_list_interface
+      when '8'
+        show_trains_on_station_by_type_interface
+      when '9'
+        show_wagon_list_by_train
+      when '10'
+        take_the_place_in_train_wagon
       end
       break if input == 'q'
     end
   end
-  
+
   private
-  
+
   def take_the_place_in_train_wagon
     if @trains.any?
       show_list(@trains)
@@ -58,7 +58,7 @@ class Program
       if train.wagons.any?
         puts "In train #{train.number} #{train.wagons.count} wagons, choose number of wagon:"
         wagon_index = gets.chomp.to_i
-        wagon = train.wagons[wagon_index-1]
+        wagon = train.wagons[wagon_index - 1]
         if wagon.type == PassengerTrain
           wagon.take_a_place
         else
@@ -75,7 +75,7 @@ class Program
       create_train_interface
     end
   end
-  
+
   def show_wagon_list_by_train
     if @trains.any?
       show_list(@trains)
@@ -86,7 +86,7 @@ class Program
       puts "First create train, trains: #{@trains.count}"
     end
   end
-  
+
   def show_trains_on_station_by_type_interface
     if @stations.any? && @trains.any?
       show_list(@stations)
@@ -96,7 +96,7 @@ class Program
       puts "First create a stations and train, stations: #{@stations.count}, trains: #{@trains.count}"
     end
   end
-  
+
   def show_station_list_interface
     if @stations.any?
       @stations.each do |station|
@@ -109,7 +109,7 @@ class Program
       puts "First create a stations, stations count: #{@stations.count}"
     end
   end
-  
+
   def move_train_on_the_route_interface
     if @trains.any?
       show_list(@trains)
@@ -128,7 +128,7 @@ class Program
       puts "First create a train, trains count: #{@trains.count}"
     end
   end
-  
+
   def remove_wagon_from_train_interface
     if @trains.any?
       show_list(@trains)
@@ -138,7 +138,7 @@ class Program
       puts "First create a train, trains count: #{@trains.count}"
     end
   end
-  
+
   def add_wagon_to_train_interface
     if @trains.any?
       show_list(@trains)
@@ -151,7 +151,7 @@ class Program
       create_train_interface
     end
   end
-  
+
   def assign_route_to_train_interface
     if @trains.any? && @routes.any?
       show_list(@routes)
@@ -163,7 +163,7 @@ class Program
       puts "First create a train or route; trains: #{@trains.count}, routes: #{@routes.count}"
     end
   end
-  
+
   def create_route_and_station_management_interface
     puts '1. Create a route'
     puts '2. Delete a route'
@@ -171,27 +171,27 @@ class Program
     puts '4. Remove station'
     puts '5. Create station'
     input = gets.chomp.to_i
-    
+
     case input
-      when 1
-        create_route_interface
-      when 2
-        delete_route_interface
-      when 3
-        add_station_to_route_interface
-      when 4
-        remove_station_from_route_interface
-      when 5
-        create_station_interface
+    when 1
+      create_route_interface
+    when 2
+      delete_route_interface
+    when 3
+      add_station_to_route_interface
+    when 4
+      remove_station_from_route_interface
+    when 5
+      create_station_interface
     end
   end
-  
+
   def create_station_interface
-    name = get_station_name
+    name = station_name
     create_station(name)
     show_list(@stations)
   end
-  
+
   def remove_station_from_route_interface
     if @routes.any? && @stations.any?
       show_list(@routes)
@@ -204,7 +204,7 @@ class Program
       puts "Do not enough routes or stations; routes: #{@routes.count}, stations: #{@stations.count}"
     end
   end
-  
+
   def add_station_to_route_interface
     if @routes.any?
       if @stations.count.zero?
@@ -225,7 +225,7 @@ class Program
       puts 'First create a route'
     end
   end
-  
+
   def delete_route_interface
     if @routes.any?
       show_list(@routes)
@@ -238,80 +238,78 @@ class Program
       puts 'Nothing to delete'
     end
   end
-  
+
   def create_route_interface
     while @stations.count < 2
       puts 'There are not enough stations'
-      name = get_station_name
+      name = station_name
       create_station(name)
     end
     show_list(@stations)
     puts 'Select the start station'
-    start = gets.chomp.to_i
+    from_start = gets.chomp.to_i
     puts 'Select the end station'
-    _end = gets.chomp.to_i
-    create_route(@stations[start], @stations[_end])
+    to_end = gets.chomp.to_i
+    create_route(@stations[from_start], @stations[to_end])
     show_list(@routes)
   end
-  
+
   def create_train_interface
     puts 'Enter the train number(format: 123-AA or QWE12):'
     number = gets.chomp
     puts 'Choose type: 1 for cargo, 2 for passenger:'
     type = gets.chomp.to_i
     train = create_train(number, type)
-    unless train.nil?
-      puts "Train number #{train.number} was successfully created."
-    end
+    puts "Train number #{train.number} was successfully created." unless train.nil?
   end
-  
-  def get_station_name
+
+  def station_name
     puts 'Enter the name of station.'
     gets.chomp
   end
-  
+
   def show_list(array)
     puts ''
     array.each.with_index { |element, i| puts "#{i}: #{element}" }
     puts ''
   end
-  
+
   def create_train(number, type)
     @trains << CargoTrain.new(number) if type == 1
     @trains << PassengerTrain.new(number) if type == 2
   rescue ValidationError => e
     puts "#{e.message}: #{e.obj}"
   end
-  
+
   def create_station(name)
     @stations << Station.new(name)
   rescue ValidationError => e
     puts "#{e.message}: #{e.obj}"
   end
-  
-  def create_route(start, _end)
-    @routes << Route.new(start, _end)
+
+  def create_route(from_start, to_end)
+    @routes << Route.new(from_start, to_end)
   rescue ValidationError => e
     puts "#{e.message}: #{e.obj}"
   end
-  
+
   def delete_route(number)
     route = @routes[number]
     @routes.delete(route)
   end
-  
+
   def add_station_to_route(route_index, station_index)
     route = @routes[route_index]
     station = @stations[station_index]
     route.add_station(station)
   end
-  
+
   def remove_station_from_route(route_index, station_index)
     route = @routes[route_index]
     station = @routes[route_index].stations[station_index]
     route.delete_station(station)
   end
-  
+
   def assign_train_on_route(route_index, train_index)
     train = @trains[train_index]
     train.route = @routes[route_index]
@@ -321,32 +319,32 @@ class Program
     station.add_train(train)
     puts "Train #{train} was assign on the station #{station} from route #{route}"
   end
-  
+
   def add_wagon_to_train(train_index, capacity)
     train = @trains[train_index]
     wagon = create_wagon(train, capacity)
     train.add_wagon(wagon)
     puts "Added wagon with #{wagon.free_space} space."
   end
-  
+
   def create_wagon(train, capacity)
     wagon = CargoWagon.new(capacity) if train.class == CargoTrain
     wagon = PassengerWagon.new(capacity) if train.class == PassengerTrain
     wagon
   end
-  
+
   def remove_wagon_from_train(train_index)
     train = @trains[train_index]
     train.remove_wagon
     puts 'Wagon removed' if train.wagons.count > 0
   end
-  
+
   def remove_train_from_station(train_index)
     train = @trains[train_index]
     station = train.current_station
     station.move train
   end
-  
+
   def move_train_to_the_next_station(train_index)
     train = @trains[train_index]
     next_station = train.next_station
@@ -359,7 +357,7 @@ class Program
       puts "Train: #{train} goes from: #{train.current_station} to: #{next_station}"
     end
   end
-  
+
   def move_train_to_the_previous_station(train_index)
     train = @trains[train_index]
     previous_station = train.previous_station
@@ -372,7 +370,7 @@ class Program
       puts "Train: #{train} goes from: #{train.current_station} to: #{previous_station}"
     end
   end
-  
+
   def show_trains_on_station(station_index)
     station = @stations[station_index]
     station.each_train.with_index(1) do |train, index|
@@ -380,7 +378,7 @@ class Program
       show_wagon_list(train)
     end
   end
-  
+
   def show_wagon_list(train)
     train.each_wagon.with_index(1) do |wagon, index|
       puts "wagon number: #{index}, type: #{wagon.type}, free space: #{wagon.free_space}"
